e47abf9d2bbe22a57e47d4fe70c47e41d47707dc
dviramontes <dviramontes@gmail.com>
2019-08-10 16:03:29 -0600

    update: css

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useRef } from 'react';
+import * as d3 from 'd3';
+
 import './App.css';
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let ref = useRef(null)
+    const H = 600
+    const W = 800
+
+    useEffect(() => {
+        d3.select(ref.current)
+            .append('circle')
+            .attr('r', 5)
+            .attr('cx', W / 2)
+            .attr('cy', H / 2)
+            .attr('fill', 'red')
+    })
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <pre> // </pre>
+            </header>
+
+            <svg id="viz-mount" width={W} height={H} ref={ref}></svg>
+
+            <header className="App-footer">
+                <pre>
+                  <p><small>CRBN_FT_PRNT</small></p>
+                  <p><small>THE BRIGHT WWWEB × MMMANYFOLD + WCOR</small></p>
+                </pre>
+            </header>
+        </div>
+    );
 }
 
 export default App;
