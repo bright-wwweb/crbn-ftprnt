636740edfe1c07cc80247f5cc8241ad8eb1388e8
dviramontes <dviramontes@gmail.com>
2019-08-11 15:37:19 -0600

    wip: add python serial code

--- arduino/motion_detect/motion_detect.ino
+++ arduino/motion_detect/motion_detect.ino
@@ -6,14 +6,13 @@ long unsigned int lowIn;
 
 //the amount of milliseconds the sensor has to be low
 //before we assume all motion has stopped
-long unsigned int pause = 5000;
+long unsigned int pause = 100;
 
 boolean lockLow = true;
 boolean takeLowTime;
 
 int pirPin = 3;    //the digital pin connected to the PIR sensor's output
 int ledPin = 13;
-
 
 /////////////////////////////
 //SETUP
@@ -47,6 +46,7 @@ void loop() {
       Serial.print("motion detected at ");
       Serial.print(millis() / 1000);
       Serial.println(" sec");
+      Serial.println("Hello From Arduino!");
       delay(50);
     }
     takeLowTime = true;
@@ -65,7 +65,7 @@ void loop() {
       //makes sure this block of code is only executed again after
       //a new motion sequence has been detected
       lockLow = true;
-      Serial.print("motion ended at ");      //output
+      Serial.print("motion  ended at ");      //output
       Serial.print((millis() - pause) / 1000);
       Serial.println(" sec");
       delay(50);
