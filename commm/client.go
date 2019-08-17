13f17c4e05308f8fee8b140cbabac3afe7d8619b
dviramontes <dviramontes@gmail.com>
2019-08-17 16:07:27 -0600

    wip: ws sever 2

--- commm/client.go
+++ commm/client.go
@@ -3,6 +3,8 @@ package main
 import "github.com/gorilla/websocket"
 
 type Client struct {
+	// hub
+	hub *Hub
 	// WS conn
 	conn *websocket.Conn
 	// Buffered channel of outbound messages
--- commm/main.go
+++ commm/main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/tarm/serial"
 	"log"
 	"net/http"
+	"time"
 )
 
 var upgrader = websocket.Upgrader{} // use defaults
@@ -20,35 +21,83 @@ func main() {
 	c0 := newSerialConn("/dev/cu.usbmodem14301", 9600)
 	//c1 := newSerialConn("/dev/cu.usbmodem14301", 9601)
 	//c2 := newSerialConn("/dev/cu.usbmodem14301", 9602)
+	hub := newHub()
 
-	s, err := serial.OpenPort(c0)
+	go serialListen(c0)
+	go hub.run()
+
+	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("pong"))
+	})
+
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(hub, w, r)
+	})
+
+	if err := http.ListenAndServe(":4000", nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func serialListen(c *serial.Config) {
+	s, err := serial.OpenPort(c)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	buf := make([]byte, 128)
-	go func() {
-		for {
-			n, err := s.Read(buf)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			log.Printf("%s\n", string(buf[:n]))
+	for {
+		n, err := s.Read(buf)
+		if err != nil {
+			log.Fatal(err)
 		}
+
+		log.Printf("%s\n", string(buf[:n]))
+	}
+}
+
+func serveWs(hub *Hub, w http.ResponseWriter, r *http.Request)  {
+	ticker := time.NewTicker(time.Second * 30)
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	client := &Client{hub: hub, conn: conn, send: make(chan []byte, 256)}
+	client.hub.register <- client
+
+	defer func() {
+		ticker.Stop()
+		client.conn.Close()
 	}()
 
-	http.HandleFunc("/ws", func(writer http.ResponseWriter, request *http.Request) {
-		_, err := upgrader.Upgrade(w, r, nil)
-		if err != nil {
-			log.Println(err)
-			return
+	for {
+
+		select {
+
+		case message, ok := <- client.send:
+			client.conn.SetWriteDeadline(time.Now().Add(10 * time.Second))
+			if !ok {
+				client.conn.WriteMessage(websocket.CloseMessage, []byte{})
+				return
+			}
+
+			w, err := client.conn.NextWriter(websocket.TextMessage)
+			if err != nil {
+				return
+			}
+			w.Write(message)
+
+			if err := w.Close(); err != nil {
+				return
+			}
+
+		case <-ticker.C:
+			client.conn.SetWriteDeadline(time.Now().Add(10 * time.Second))
+			if err := client.conn.WriteMessage(websocket.PingMessage, nil); err != nil {
+				return
+			}
 		}
-
-		//client := &Client{conn: conn, send: make(chan []byte, 256)}
-	})
-
-	if err := http.ListenAndServe(":4000", nil); err != nil {
-		log.Fatal(err)
 	}
 }
