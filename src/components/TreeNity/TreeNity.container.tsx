12ee23c5c8995a2c2287af47ef90cf1f7a609600
David A. Viramontes <dviramontes@gmail.com>
2019-08-18 16:07:09 -0600

    fix: spacing, btn layout and commented out code

--- src/components/TreeNity/TreeNity.container.tsx
+++ src/components/TreeNity/TreeNity.container.tsx
@@ -27,15 +27,17 @@ const TreeNity: FC<Props> = ({
   width, height, signal, signalCount, handleClickSignal
 }) => {
 
-  const[graph, setGraph] = useState<d3Graph>({ nodes: [
-    {
+  const [graph, setGraph] = useState<d3Graph>({
+    nodes: [{
       id: "ORIGIN",
       group: 4
     }
-  ], links: [] });
+    ],
+    links: [],
+  });
 
-  const[source, setSource] = useState<number | null>(null);
-  const[target, setTarget] = useState<number>(0)
+  const [source, setSource] = useState<number | null>(null);
+  const [target, setTarget] = useState<number>(0)
 
   const [treeState, setTreeState] = useState<any>({
     A: {},
@@ -44,7 +46,7 @@ const TreeNity: FC<Props> = ({
   });
 
   let simulation: any = d3.forceSimulation(graph.nodes)
-    .force("link", d3.forceLink().id(function (d: any) {
+    .force("link", d3.forceLink().id(function(d: any) {
       return d.id
     }))
     .force("charge", d3.forceManyBody().strength(-100))
@@ -59,46 +61,43 @@ const TreeNity: FC<Props> = ({
   }, [signalCount])
 
   useEffect(() => {
-    // if (graph.nodes[0].x === 0) {
-    // console.log(graph)
     const node = d3.selectAll(".node")
     const link = d3.selectAll(".link")
     const label = d3.selectAll(".label");
 
     function ticked() {
       link
-        .attr("x1", function (d: any) {
+        .attr("x1", function(d: any) {
           return d.source.x
         })
-        .attr("y1", function (d: any) {
+        .attr("y1", function(d: any) {
           return d.source.y
         })
-        .attr("x2", function (d: any) {
+        .attr("x2", function(d: any) {
           return d.target.x
         })
-        .attr("y2", function (d: any) {
+        .attr("y2", function(d: any) {
           return d.target.y
         })
       node
-        .attr("cx", function (d: any) {
+        .attr("cx", function(d: any) {
           return d.x = Math.max(5, Math.min(width - 5, d.x))
         })
-        .attr("cy", function (d: any) {
+        .attr("cy", function(d: any) {
           return d.y = Math.max(5, Math.min(height - 5, d.y))
         })
 
       label
-        .attr("x", function (d: any) {
+        .attr("x", function(d: any) {
           return d.x + 5;
         })
-        .attr("y", function (d: any) {
+        .attr("y", function(d: any) {
           return d.y + 5;
         })
     }
 
     simulation.nodes(graph.nodes).on("tick", ticked)
     simulation.force("link").links(graph.links)
-    // }
   }, [target])
 
   function _handleNewSignal() {
@@ -119,16 +118,21 @@ const TreeNity: FC<Props> = ({
         }
       }
     }
+
     const targetObj = {
       source: newSourceId,
       leftChild,
       rightChild,
     }
+
     const newT = {
       ...treeState,
-      [signal]: { ...treeState[signal], [newTargetId]: targetObj }
+      [signal]: {
+        ...treeState[signal],
+        [newTargetId]: targetObj
+      }
     }
-    console.log("NEWTARGET", signal, newTargetId, newT)
+
     setSource(newSourceId)
     setTarget(newTargetId)
     setTreeState(newT)
@@ -137,31 +141,35 @@ const TreeNity: FC<Props> = ({
 
   // signal is (A, B, or C) & it represents the tree that is being updated
   function _createNodeEntry() {
-    let group;
+    let group: number
+    let newGraph: d3Graph
+
     switch (signal) {
       case "A":
-        group = 1; break
+        group = 1;
+        break
       case "B":
-        group = 2; break
+        group = 2;
+        break
       case "C":
-        group = 3; break
+        group = 3;
+        break
       default:
         throw new Error(`Gurrlll this ain't A B nor C. Check yoself. Wut came thru for signal: ${signal}`)
     }
-    let newGraph;
-    console.log(signal)
+
     if (signal) {
       newGraph = {
         nodes: graph.nodes.concat({
           id: `${signal}${target}`,
-          group}),
+          group,
+        }),
         links: _createLinkEntry()
       }
     } else {
       newGraph = graph
     }
 
-    console.log("GRAPH", newGraph)
     simulation.stop()
     setGraph(newGraph)
     simulation.restart()
@@ -189,16 +197,18 @@ const TreeNity: FC<Props> = ({
   }
 
   return (
-    <>
-      <button onClick={() => {handleClickSignal("A")}}>ADD BLUE NODE</button>
-      <button onClick={() => {handleClickSignal("B")}}>ADD RED NODE</button>
-      <button onClick={() => {handleClickSignal("C")}}>ADD GREEN NODE</button>
+    <div id="viz-container">
       <svg className="viz-mount" width={width} height={height}>
-        <Links links={graph.links} />
-        <Nodes nodes={graph.nodes} />
-        <Labels nodes={graph.nodes} />
+        <Links links={graph.links}/>
+        <Nodes nodes={graph.nodes}/>
+        <Labels nodes={graph.nodes}/>
       </svg>
-    </>
+      <div id="btn-container">
+        <button onClick={() => {handleClickSignal("A")}}>ADD BLUE NODE</button>
+        <button onClick={() => {handleClickSignal("B")}}>ADD RED NODE</button>
+        <button onClick={() => {handleClickSignal("C")}}>ADD GREEN NODE</button>
+      </div>
+    </div>
   )
 };
 
